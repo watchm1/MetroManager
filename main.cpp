9b60d8216d53f0303daad5975868ede222c38059
merve haciabbasoglu <dev.mervehaciabbasoglu@gmail.com>
2022-12-14 13:36:37 +0300

    refactor about write data function from NOSqlService

--- main.cpp
+++ main.cpp
@@ -8,14 +8,21 @@ int main() {
   
   
   Line lineData;
+  
   NOSqlService<RootObject<Line>> lineService;
   lineData.ModelUniqueID = "test1";
   lineData.ModelName = "model 1";
   lineData.isActive = 1;
+  
+  Line lineData2;
+  lineData2.ModelName = "model 2";
+  lineData2.ModelUniqueID = "test 2";
+  lineData2.isActive = 0;
+  
   RootObject<Line> root;
   root.lists.push_back(lineData); 
-  RootObject<Line> newLine = lineService.WriteData("lines.txt", root);
-  std::cout << newLine.lists[0].ModelName << std::endl;
+  root.lists.push_back(lineData2);
+  lineService.WriteData("lines.txt", root);
   
   return 0;
 }                                  
